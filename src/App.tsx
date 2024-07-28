8a9683a3606f8d23a96cadeb09ce2fca2e901436
sidratariq <sidratariq48@gmail.com>
2024-07-28 13:35:09 +0500

    challenge start

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import dpsLogo from './assets/DPS.svg';
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<div>
+				<a href="https://www.digitalproductschool.io/" target="_blank">
+					<img src={dpsLogo} className="logo" alt="DPS logo" />
+				</a>
+			</div>
+			<div className="home-card">
+				<p>Your solution goes here 😊</p>
+			</div>
+		</>
+	);
+}
+
+export default App;
